abd2afc2dea6c2a2c2e08d20bf59e2f8cc1cdef3
Pasindu Dinal <pasindu.dinal@randomsoftware.net>
2023-10-24 14:17:17 +0530

    Fix:Refactored the files

--- components/recycle/RecycleRequestCardView.jsx
+++ components/recycle/RecycleRequestCardView.jsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
-import styles from "./productCardView.style";
+import styles from "../product/productCardView.style";
 import { COLORS } from "../../constants";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
